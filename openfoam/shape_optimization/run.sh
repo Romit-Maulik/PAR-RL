120e2810af97aeb4dfebb547a7797ab7c0c0b1db
Suraj <surajp92@vt.edu>
2020-08-13 16:33:24 -0500

    openfoam folder removed

--- openfoam/shape_optimization/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -r [1-9]*
-simpleFoam > log.simpleFoam
-foamToVTK
--- openfoam/turbulence_model_coefficients/version_1/extract_field.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash          
-DIR=$1
-FOLD=$2
-LOG=$3 
-#echo $LOG
-rm ./$DIR/$LOG\_*.txt
-cat $DIR/$FOLD/$LOG | grep '(' |  tr -d '()' > ./$DIR/$LOG\_$FOLD.txt
-head -n -3 ./$DIR/$LOG\_$FOLD.txt > ./$DIR/$LOG\_$FOLD\_t.txt
